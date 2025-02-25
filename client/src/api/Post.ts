c36de864ffb9eaa5153e28fcda59605f8cf7907f
Your Name <you@example.com>
2025-02-25 20:20:26 +0300

    home commit

--- client/src/api/Post.ts
+++ client/src/api/Post.ts
@@ -1,18 +1,82 @@
 import { z } from "zod";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const PostShema = z.object({
+const PostSchema = z.object({
     id: z.string(),
     text: z.string(),
     authorId: z.string(),
     created: z.string(),
 });
 
-export type Post = z.infer<typeof PostShema>;
+export type Post = z.infer<typeof PostSchema>;
 
-export const PostList = z.array(PostShema);
+export const PostList = z.array(PostSchema);
 
 export type PostList = z.infer<typeof PostList>
 
-function fetchPostlist(): Promise {
-    return fetch("/api/posts");
+export const FetchPostListSchema = z.object({
+    list: PostList,
+})
+
+type FetchPostListResponse = z.infer<typeof FetchPostListSchema>;
+
+export function fetchPostlist(): Promise<FetchPostListResponse> {
+    return fetch("/api/posts")
+    .then((response) => response.json())
+    .then((data) => FetchPostListSchema.parse(data));
+}
+
+interface IdleRequestState {
+status: "idle;"
+}
+
+interface LoadingRequestState {
+status: "pending";
+}
+
+interface SuccesRequestState {
+status: "succes";
+data: PostList;
+}
+
+interface ErrorRequestState {
+status: "error"; 
+error: unknown;  
+}
+
+type RequestState = 
+| IdleRequestState 
+|LoadingRequestState
+|SuccesRequestState
+|ErrorRequestState
+
+export function usePostList() {
+    const [state, setState] = useState<RequestState>({status: "idle"})
+
+
+useEffect(() => {
+    if (state.status ==="pending") {
+        fetchPostlist()
+        .then((data) => {
+            setState({ status:"succes", data: data.list});
+        })
+        .catch((error) => {
+            setState({ status: "error", error});
+        });
+    }
+}, [state]);
+
+useEffect(() => {
+    setState({ status:"pending"});
+}, []);
+
+const refetch = () => {
+    setState({ status: "pending" })
+};
+
+return {
+    state, 
+    refetch,
+};
 }
--- client/src/api/User.ts
+++ client/src/api/User.ts
@@ -1,9 +1,15 @@
-import {z} from 'zod'
+import { z } from 'zod'
 
 
-export const UserShema =z.object({
+export const UserSchema =z.object({
     id: z.string(),
     username: z.string(),
 })
 
-export type User = z.infer<typeof UserShema>;
+export type User = z.infer<typeof UserSchema>;
+
+export function fetchUser(id: string): Promise<User> {
+    return fetch(`/api/users/${id}`)
+    .then((response) => response.json())
+    .then((data) => UserSchema.parse(data));
+}
