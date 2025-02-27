176ffcf9ad7c476383c6fec54a22cf6598d80716
Your Name <you@example.com>
2025-02-27 22:09:52 +0300

    отправка данных на сервер

--- client/src/api/User.ts
+++ client/src/api/User.ts
@@ -13,3 +13,34 @@ export function fetchUser(id: string): Promise<User> {
     .then((response) => response.json())
     .then((data) => UserSchema.parse(data));
 }
+
+export function registerUser(username: string, password: string): Promise<void> {
+    return fetch("/api/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({ username, password }),
+    }).then(() => undefined);
+}
+
+async function validateResponse(response: Response): Promise<Response> {
+    if(!response.ok) {
+        throw new Error(await response.text());
+    }
+    return response;
+}
+
+export function login(username: string, password: string): Promise<void> {
+    return fetch("/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({ username, password }),
+    })
+    .then(validateResponse)
+    .then(() => undefined);
+}
--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,12 +1,20 @@
 import { FC, FormEventHandler, useState } from 'react';
-
+import {login} from '../../api/User'
+import { useMutation } from '@tanstack/react-query';
 import { FormField } from '../FormField';
 import { Button } from '../Button';
 import './LoginForm.css';
+import { queryClient } from '../../api/queryClients';
 
 export const LoginForm: FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+const loginMutation = useMutation({
+  mutationFn: () => login(username, password)
+},
+queryClient
+);
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
@@ -32,7 +40,9 @@ export const LoginForm: FC = () => {
         />
       </FormField>
 
-      <Button type="submit" title="Войти" />
+      {loginMutation.error && <span>{loginMutation.error.message}</span>}
+
+      <Button type="submit" title="Войти" isLoading= {loginMutation.isPending}/>
     </form>
   );
 };
